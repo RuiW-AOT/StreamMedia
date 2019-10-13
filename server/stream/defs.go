a647c1750a7035b443a2e1f0b0ca244d893e6f89
RuiW-AOT <rwang721@gmail.com>
2019-10-13 13:05:59 -0700

    Implement upload stream

--- server/stream/defs.go
+++ server/stream/defs.go
@@ -1,6 +1,7 @@
 package main
 
 const (
-	VIDEO_DIR  = "./videos/"
-	RATE_LIMIT = 2
+	VIDEO_DIR       = "./videos/"
+	RATE_LIMIT      = 20
+	MAX_UPLOAD_SIZE = 1024 * 1024 * 50 // 50MB
 )
--- server/stream/handlers.go
+++ server/stream/handlers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -16,6 +19,7 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 	video, err := os.Open(vl)
 	if err != nil {
 		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error, cannot find video")
+		return
 	}
 
 	w.Header().Set("Content-Type", "video/mp4")
@@ -24,5 +28,35 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		sendErrorResponse(w, http.StatusBadRequest, "File is too large")
+		return
+	}
 
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		log.Printf("Open file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	fn := p.ByName("vid-id")
+
+	err = ioutil.WriteFile(VIDEO_DIR+fn, data, 0666)
+	if err != nil {
+		log.Printf("Write file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Uploaded successfully")
 }

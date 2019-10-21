b61c0c03a2e6ccb14184e3b5149ca6d2fa7e120d
RuiW-AOT <rwang721@gmail.com>
2019-10-20 21:22:57 -0700

    Inital code frame of scheduler

--- server/stream/main.go
+++ server/stream/main.go
@@ -32,7 +32,6 @@ func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 	router.GET("/videos/:vid-id", streamHandler)
 	router.POST("/videos/:vid-id", uploadHandler)
-
 	return router
 }
 
